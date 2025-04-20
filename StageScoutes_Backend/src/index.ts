46609361dd31e4f3f4e8eb621b53c62843e8eeac
bhavukarora64 <bhavukarora64@gmail.com>
2025-04-21 01:27:13 +0200

    fix: remove specific CORS origin configuration for broader access

--- StageScoutes_Backend/src/index.ts
+++ StageScoutes_Backend/src/index.ts
@@ -12,7 +12,8 @@ import venuesRoute from './routes/venuesRoute';
 
 const app = express();
 
-app.use(cors({ origin: 'https://stagescouts.bhavukarora.eu', credentials: true }));
+app.use(cors());
+
 app.options('*', cors());
 
 app.use(dbMiddleware);
